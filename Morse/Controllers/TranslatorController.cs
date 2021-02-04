8da18504084d0214dd3444beac023a9bd1b6749f
unknown <safer.andy@gmail.com>
2021-02-04 05:36:38 +0200

    Started working with morse code encoding

---
+++ Morse/Controllers/TranslatorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Morse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Morse.Controllers
+{
+    public class TranslatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Translated( string fromText)
+        {
+            Translator temp = new Translator
+            {
+                fromText = fromText
+            };
+            return View(temp);
+        }
+    }
+}
---
+++ Morse/Models/Translator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Morse.Models
+{
+    public class Translator
+    {
+        public string fromText { get; set; }
+
+        public string MorseCode()
+        {
+            Initiate();
+            return @"~/sound/candy.mp3";
+        }
+        public string Initiate()
+        {
+            string path = @"wwwroot\sound\";
+            var v = File.Create(@"wwwroot\sound\candy.mp3");
+            char[] textToLetters = fromText.ToCharArray();
+            for (int i = 0; i < textToLetters.Length; i++) { 
+                v.Write(File.ReadAllBytes(path + textToLetters[i] + ".mp3"));
+            }
+            v.Close();
+            return null;
+        }
+       
+    }
+}
